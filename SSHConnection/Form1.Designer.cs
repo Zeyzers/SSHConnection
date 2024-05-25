d5a6159c89d480f85b1581fe7aa8207052d951f0
Zeyzer <72913568+Zeyzers@users.noreply.github.com>
2024-05-25 14:49:38 +0200

    Aggiungere i file di progetto.

---
+++ SSHConnection/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace SSHConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.txtHostname = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConnect_Click = new System.Windows.Forms.Button();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.txtCommand = new System.Windows.Forms.TextBox();
+            this.btnSendCommand = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnDisconnect_Click = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(236, 450);
+            this.splitter1.TabIndex = 0;
+            this.splitter1.TabStop = false;
+            // 
+            // txtHostname
+            // 
+            this.txtHostname.Location = new System.Drawing.Point(15, 35);
+            this.txtHostname.Name = "txtHostname";
+            this.txtHostname.Size = new System.Drawing.Size(200, 20);
+            this.txtHostname.TabIndex = 1;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(15, 80);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(200, 20);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Hostname";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Username";
+            // 
+            // btnConnect_Click
+            // 
+            this.btnConnect_Click.Location = new System.Drawing.Point(140, 148);
+            this.btnConnect_Click.Name = "btnConnect_Click";
+            this.btnConnect_Click.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect_Click.TabIndex = 4;
+            this.btnConnect_Click.Text = "Connect";
+            this.btnConnect_Click.UseVisualStyleBackColor = true;
+            this.btnConnect_Click.Click += new System.EventHandler(this.btnConnect_Click_Click);
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.EnableAutoDragDrop = true;
+            this.rtbOutput.Location = new System.Drawing.Point(242, 19);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.ReadOnly = true;
+            this.rtbOutput.Size = new System.Drawing.Size(546, 365);
+            this.rtbOutput.TabIndex = 7;
+            this.rtbOutput.Text = "";
+            // 
+            // txtCommand
+            // 
+            this.txtCommand.Location = new System.Drawing.Point(243, 391);
+            this.txtCommand.Name = "txtCommand";
+            this.txtCommand.ReadOnly = true;
+            this.txtCommand.Size = new System.Drawing.Size(428, 20);
+            this.txtCommand.TabIndex = 5;
+            this.txtCommand.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCommand_KeyPress);
+            // 
+            // btnSendCommand
+            // 
+            this.btnSendCommand.Enabled = false;
+            this.btnSendCommand.Location = new System.Drawing.Point(677, 391);
+            this.btnSendCommand.Name = "btnSendCommand";
+            this.btnSendCommand.Size = new System.Drawing.Size(111, 20);
+            this.btnSendCommand.TabIndex = 6;
+            this.btnSendCommand.Text = "Send";
+            this.btnSendCommand.UseVisualStyleBackColor = true;
+            this.btnSendCommand.Click += new System.EventHandler(this.btnSendCommand_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(15, 122);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(200, 20);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPassword_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Password";
+            // 
+            // btnDisconnect_Click
+            // 
+            this.btnDisconnect_Click.Enabled = false;
+            this.btnDisconnect_Click.Location = new System.Drawing.Point(677, 417);
+            this.btnDisconnect_Click.Name = "btnDisconnect_Click";
+            this.btnDisconnect_Click.Size = new System.Drawing.Size(111, 23);
+            this.btnDisconnect_Click.TabIndex = 12;
+            this.btnDisconnect_Click.Text = "Disconnect";
+            this.btnDisconnect_Click.UseVisualStyleBackColor = true;
+            this.btnDisconnect_Click.Click += new System.EventHandler(this.btnDisconnect_Click_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDisconnect_Click);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.btnSendCommand);
+            this.Controls.Add(this.txtCommand);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.btnConnect_Click);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtHostname);
+            this.Controls.Add(this.splitter1);
+            this.Name = "Form1";
+            this.Text = "SSH Connection";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.TextBox txtHostname;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConnect_Click;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.TextBox txtCommand;
+        private System.Windows.Forms.Button btnSendCommand;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnDisconnect_Click;
+    }
+}
+
---
+++ SSHConnection/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Renci.SshNet;
+
+
+namespace SSHConnection
+{
+    public partial class Form1 : Form
+    {
+        private SshClient sshClient;
+        private ShellStream shellStream;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string hostname = txtHostname.Text;
+                int port = 22;
+                string username = txtUsername.Text;
+                string password = txtPassword.Text;
+
+                sshClient = new SshClient(hostname, port, username, password);
+                sshClient.Connect();
+                shellStream = sshClient.CreateShellStream("xterm", 80, 24, 800, 600, 1024);
+
+                rtbOutput.AppendText("Connected to SSH server.\n");
+                txtCommand.ReadOnly = false;
+                btnSendCommand.Enabled = true;
+                btnConnect_Click.Enabled = false;
+                btnDisconnect_Click.Enabled = true;
+                txtHostname.Enabled = false;
+                txtUsername.Enabled = false;
+                txtPassword.Enabled = false;
+                
+            }
+            catch (Exception ex)
+            {
+                rtbOutput.AppendText($"Error: {ex.Message}\n");
+            }
+
+        }
+
+        private void btnDisconnect_Click_Click(object sender, EventArgs e)
+        {
+
+            if (sshClient != null && sshClient.IsConnected)
+            {
+                sshClient.Disconnect();
+                rtbOutput.AppendText("Disconnected from SSH server.\n");
+                txtCommand.ReadOnly = true;
+                btnSendCommand.Enabled = false;
+                btnConnect_Click.Enabled = true;
+                btnDisconnect_Click.Enabled = false;
+                txtHostname.Enabled = true;
+                txtUsername.Enabled = true;
+                txtPassword.Enabled = true;
+            }
+
+        }
+
+        private void btnSendCommand_Click(object sender, EventArgs e)
+        {
+            if (sshClient != null && sshClient.IsConnected)
+            {
+                string command = txtCommand.Text;
+                WriteToShellStream(command);
+
+                txtCommand.Text = "";
+
+            }
+            else
+            {
+                rtbOutput.AppendText("Not connected to any SSH server.\n");
+            }
+        }
+
+        private string StripAnsiSequences(string input)
+        {
+            // This regex pattern matches a wide range of ANSI escape sequences
+            var ansiRegex = new Regex(@"\x1B[@-_][0-?]*[ -/]*[@-~]");
+            return ansiRegex.Replace(input, string.Empty);
+        }
+
+        private void WriteToShellStream(string command)
+        {
+            shellStream.WriteLine(command);
+            shellStream.Flush();
+
+            var result = ReadShellStream(command);
+            var cleanResult = StripAnsiSequences(result);  // Strip the ANSI sequences
+            AppendTextToOutput(cleanResult);
+        }
+
+        private string ReadShellStream(string command)
+        {
+            var output = new StringBuilder();
+            string line;
+            while ((line = shellStream.ReadLine(TimeSpan.FromMilliseconds(100))) != null)
+            {
+                output.AppendLine(line);
+            }
+
+            var result = output.ToString();
+
+            // Remove the command echo from the result
+            var commandEchoIndex = result.IndexOf(command);
+            if (commandEchoIndex >= 0)
+            {
+                result = result.Remove(commandEchoIndex, command.Length).Trim();
+            }
+
+            return result;
+        }
+
+        private void AppendTextToOutput(string text)
+        {
+            rtbOutput.AppendText(text);
+            ScrollToBottom();
+        }
+
+        private void ScrollToBottom()
+        {
+            rtbOutput.SelectionStart = rtbOutput.Text.Length;
+            rtbOutput.ScrollToCaret();
+        }
+
+
+        private void txtPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter) // Correct way to detect Enter key
+            {
+                btnConnect_Click_Click(sender, e);
+                e.Handled = true; // Prevent the beep sound on Enter key press
+            }
+        }
+
+        private void txtCommand_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter) // Correct way to detect Enter key
+            {
+                btnSendCommand_Click(sender, e);
+                e.Handled = true; // Prevent the beep sound on Enter key press
+            }
+        }
+    }
+}
